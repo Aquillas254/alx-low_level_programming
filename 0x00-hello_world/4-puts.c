f48c19620aa50667ee714492c27a85c9f50f42bc
Aquillas254 <masindeaquillas@gmail.com>
2022-12-08 11:31:54 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -9,17 +9,10 @@
  *    *          * Return: zero
  *
  *     */
-
 int main(void)
-
 {
-
 			char Mystr[] = "\"Programming is like building a multilingual puzzle";
 
-
-
-						puts(Mystr);
-
-										return (0);
-
+					puts(Mystr);
+						return (0);
 }
